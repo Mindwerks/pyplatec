8c155acd721181ed4fa4c38435da9a5386dd7393
Bret Curtis <psi29a@gmail.com>
2025-03-24 12:09:05 +0100

    give this a try

--- setup.py
+++ setup.py
@@ -38,5 +38,7 @@ setup (name = 'PyPlatec',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ])
